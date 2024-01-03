9221ae7c113420cac128dde389690eccd2c27c2e
plaze <banikakash131@gamil.com>
2024-01-03 10:30:27 +0530

    feat: add root routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import { PORT } from "./config/port.js";
+import router from "./routes/api/book.js";
+
 const app = express();
+
+app.use("/", router);
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT:${PORT}`);
