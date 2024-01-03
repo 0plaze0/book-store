c68f6fdec81c412001fccd43588253ed9b049649
plaze <banikakash131@gamil.com>
2024-01-03 18:41:21 +0530

    feat(controller): add book controller

---
+++ backend/controller/bookController.js
@@ -0,0 +1,22 @@
+import Book from "../model/Book";
+
+const createBook = async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({ message: "Please Enter valid data" });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const result = await Book.create(newBook);
+    res.status(200).send(result);
+  } catch (err) {
+    console.log(err.msg);
+    res.status(500).send({ message: err.message });
+  }
+};
+
+export default { createBook };
