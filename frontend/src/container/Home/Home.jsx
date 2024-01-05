8550dc38d65f6755833b117238a95cf474e95e05
plaze <banikakash131@gamil.com>
2024-01-05 13:20:50 +0530

    feat: add base url

--- frontend/src/container/Home/Home.jsx
+++ frontend/src/container/Home/Home.jsx
@@ -1,14 +1,26 @@
-import React, { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Spinner } from "./../../components";
+import bookApi from "../../api/bookApi";
+
 const Home = () => {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await await fe;
+      const data = await bookApi.get("/books");
+
+      setBooks(data.data.book);
     };
+    fetchData();
   }, []);
-  return <div></div>;
+
+  return (
+    <div>
+      {books.map((book) => (
+        <li key={book._id}>{book.title}</li>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
