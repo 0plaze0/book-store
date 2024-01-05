8677cfdfbd48e8df23ef621bc9603ca0db5afb89
plaze <banikakash131@gamil.com>
2024-01-05 11:28:32 +0530

    fix: wrong development domain

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,13 +1,15 @@
-const whitelist = ["http://localhost:3500", "http://localhost:5173/"];
+const whitelist = ["http://localhost:3500", "http://localhost:5173"];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) != -1) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed"));
     }
   },
+  // methods: ["GET", "POST", "PUT", "DELETE"],
+  // allowedHeaders: ["Content-Type"],
 };
 
 export default corsOptions;
