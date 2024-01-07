1a566e01cd9ca7093cc3fbb36583294d0d962c3a
plaze <banikakash131@gamil.com>
2024-01-07 19:07:03 +0530

    refactor: seperate book list component

---
+++ frontend/src/components/BookList/BookList.jsx
@@ -0,0 +1,55 @@
+import { Link } from "react-router-dom";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineDelete } from "react-icons/md";
+
+const BookList = ({ books }) => {
+  return (
+    <table className="w-full border-separate border-spacing-2">
+      <thead>
+        <tr>
+          <th className="border border-slate-700 rounded-md">No.</th>
+          <th className="border border-slate-700 rounded-md">Title</th>
+          <th className="border border-slate-700 rounded-md max-md:hidden">
+            Autor
+          </th>
+          <th className="border border-slate-700 rounded-md max-md:hidden">
+            Publish Year
+          </th>
+          <th className="border border-slate-700 rounded-md">Operations</th>
+        </tr>
+      </thead>
+      <tbody>
+        {books.map((book, index) => (
+          <tr key={book._id} className="h-8">
+            <td className="flex justify-center border border-slate-700 rounded-md">
+              {index + 1}
+            </td>
+            <td className="border border-slate-700 rounded-md">{book.title}</td>
+            <td className="border border-slate-700 rounded-md max-md:hidden">
+              {book.author}
+            </td>
+            <td className="border border-slate-700 rounded-md max-md:hidden">
+              {book.publishYear}
+            </td>
+            <td className="border border-slate-700 rounded-md text-center">
+              <div className="flex justify-center gap-x-4">
+                <Link to={`/books/details/${book._id}`}>
+                  <BsInfoCircle className="text-2xl text-blue-700" />
+                </Link>
+                <Link to={`/books/edit/${book._id}`}>
+                  <AiOutlineEdit className="text-2xl text-yellow-400" />
+                </Link>
+                <Link to={`/books/delete/${book._id}`}>
+                  <MdOutlineDelete className="text-2xl text-red-700" />
+                </Link>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default BookList;
--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -1,2 +1,3 @@
 export { default as Spinner } from "./Spinner/Spinner";
 export { default as BackButton } from "./BackButton/BackButton";
+export { default as BookList } from "./BookList/BookList";
