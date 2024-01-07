8f89ab40d8bdad9e03c0b98fb29d6af53234524b
plaze <banikakash131@gamil.com>
2024-01-07 18:36:05 +0530

    feat: delete book item

--- frontend/src/container/DeleteBook/DeleteBook.jsx
+++ frontend/src/container/DeleteBook/DeleteBook.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { BackButton, Spinner } from "./../../components";
+import bookApi from "./../../api/bookApi";
 
 const DeleteBook = () => {
-  return <div>DeleteBook</div>;
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      await bookApi.delete(`./books/${id}`);
+      setLoading(false);
+      navigate("/");
+    } catch (err) {
+      console.log(err.message);
+      alert("An error has occured");
+    }
+  };
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? <Spinner /> : " "}
+      <div className="w-full p-2 flex flex-col items-center border-2 border-sky-200 bg-grey-200">
+        <h1 className="text-2xl m-4">Are you sure you want to delete it?</h1>
+        <button
+          className="text-2xl bg-red-500 p-2 rounded-md text-white"
+          onClick={handleDelete}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default DeleteBook;
