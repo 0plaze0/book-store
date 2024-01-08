86e567a6ecf416c772714205d09aa5f89baad4b3
plaze <banikakash131@gamil.com>
2024-01-08 09:55:01 +0530

    feat: toogle between table and card

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -1,3 +1,4 @@
 export { default as Spinner } from "./Spinner/Spinner";
 export { default as BackButton } from "./BackButton/BackButton";
 export { default as BookList } from "./BookList/BookList";
+export { default as BookCard } from "./BookCard/BookCard";
--- frontend/src/container/Home/Home.jsx
+++ frontend/src/container/Home/Home.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 import { MdOutlineAddBox } from "react-icons/md";
 
-import { Spinner, BookList } from "./../../components";
+import { Spinner, BookList, BookCard } from "./../../components";
 import bookApi from "../../api/bookApi";
 
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
-
+  const [toogle, setToogle] = useState("table");
   useEffect(() => {
     setLoading(true);
     const fetchData = async () => {
@@ -28,13 +28,33 @@ const Home = () => {
 
   return (
     <div className="p-4">
+      <div className="flex justify-center m-4">
+        <button
+          className="flex bg-sky-500  hover:bg-sky-400 text-white p-2 rounded-md mr-4"
+          onClick={() => setToogle("table")}
+        >
+          Table
+        </button>
+        <button
+          className="flex bg-sky-500  hover:bg-sky-400 text-white p-2 rounded-md"
+          onClick={() => setToogle("card")}
+        >
+          Card
+        </button>
+      </div>
       <div className="flex justify-between items-center">
         <div className="text-3xl my-8">Books List</div>
         <Link to="/books/create">
           <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
       </div>
-      {loading ? <Spinner /> : <BookList books={books} />}
+      {loading ? (
+        <Spinner />
+      ) : toogle === "table" ? (
+        <BookList books={books} />
+      ) : (
+        <BookCard books={books} />
+      )}
     </div>
   );
 };
